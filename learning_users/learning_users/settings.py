2b960f68f1895e2b2b9a105e487674172227dfaa
Calvin Smith <calvin.smith@lindex.com>
2024-07-26 13:20:15 +0200

    first commit

--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-sj5u)ry7*wrfd-m8*b(4tu*(t^j3%1d^waeb#d0n74anmmsc0#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['calvinnsmith.pythonanywhere.com']
 
 
 # Application definition
